26c55d3c071378b3f54df88f34925c8b5bd4a7a5
Siddarth Kumar <siddarthkay@gmail.com>
2025-01-31 00:25:22 +0530

    nix: add flake for builds

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Flake file for status network bridge website ";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = forEachSystem (system: import nixpkgs { inherit system; });
+    in
+    rec {
+      formatter = forEachSystem (system: pkgsFor.${system}.nixpkgs-fmt);
+
+      devShells = forEachSystem (system: {
+        default = pkgsFor.${system}.mkShellNoCC {
+          packages = with pkgsFor.${system}.buildPackages; [
+            git
+            openssh
+            yarn # 1.22.22
+            nodejs_20 # v20.15.1
+            ghp-import # 2.1.0
+          ];
+        };
+      });
+    };
+}
