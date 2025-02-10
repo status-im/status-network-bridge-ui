771e8976ab62069ab03fe0c16a311284b938f4cc
Filip Pajic <42151917+pajicf@users.noreply.github.com>
2025-02-10 09:37:22 +0100

    feat: Wallet login simplification (#5)
    * feat(bridge-ui): Simplify the wallet connection modal (#509)
    
    * feat(bridge-ui): Simplify the wallet connection modal
    
    * Fix copy on metadata
    
    Co-authored-by: The Dark Jester <thedarkjester@users.noreply.github.com>
    Signed-off-by: Alain Nicolas <alainncls@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alain Nicolas <alainncls@users.noreply.github.com>
    Co-authored-by: The Dark Jester <thedarkjester@users.noreply.github.com>
    
    (cherry picked from commit 06294fda72e3d97088b41facf5f6acddc7baa01d)
    
    * fix: merge conflicts
    
    * chore: remove extra files from merge
    
    ---------
    
    Co-authored-by: Alain Nicolas <alainncls@users.noreply.github.com>

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,20 +1,11 @@
-import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { http, injected } from "@wagmi/core";
-import {coinbaseWallet, walletConnect} from "@wagmi/connectors";
+import { http } from "@wagmi/core";
 import { config } from "./config";
 import { chains } from "./wagmiChains"
 import {availableChainIds, CHAIN_ID_TO_DEFAULT_RPC} from "@/utils/constants";
 import {Transport} from "viem";
+import {WagmiAdapter} from "@reown/appkit-adapter-wagmi";
 
 if (!config.walletConnectId) throw new Error("Project ID is not defined");
-
-const metadata = {
-  name: "Status Network Bridge",
-  description: `Status Network Bridge is a bridge solution, providing secure and efficient cross-chain transactions between Layer 1 and Status networks.
-  Discover the future of blockchain interaction with Status Network Bridge.`,
-  url: "https://bridge.status.network",
-  icons: [],
-};
 
 const getTransports = () => {
   const ts: Record<number, Transport> = {};
@@ -26,25 +17,15 @@ const getTransports = () => {
 }
 const transports = getTransports()
 
-export const wagmiConfig = defaultWagmiConfig({
-  chains,
+export const wagmiAdapter = new WagmiAdapter({
+  networks: chains,
   projectId: config.walletConnectId,
-  metadata,
   multiInjectedProviderDiscovery: true,
   ssr: true,
-  enableEIP6963: true,
   batch: {
     multicall: true,
   },
-  connectors: [
-    walletConnect({
-      projectId: config.walletConnectId,
-      showQrModal: false,
-    }),
-    injected({ shimDisconnect: true }),
-    coinbaseWallet({
-      appName: "Status Network Bridge",
-    }),
-  ],
   transports
 });
+
+export const wagmiConfig = wagmiAdapter.wagmiConfig;
--- src/contexts/web3.context.tsx
+++ src/contexts/web3.context.tsx
@@ -2,21 +2,44 @@
 
 import { ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
-import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config, wagmiConfig } from "@/config";
+import { createAppKit } from "@reown/appkit/react";
+import { wagmiAdapter } from "@/config/wagmi";
+import { chains } from "@/config/wagmiChains";
+
 
 const queryClient = new QueryClient();
 
 if (!config.walletConnectId) throw new Error("Project ID is not defined");
 
 const METAMASK_WALLET_ID = "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96"
-const STATUS_WALLET_ID = "af9a6dfff9e63977bbde28fb23518834f08b696fe8bff6dd6827acad1814c6be"
+// const STATUS_WALLET_ID = "af9a6dfff9e63977bbde28fb23518834f08b696fe8bff6dd6827acad1814c6be" @TODO Uncomment when app implements support
 
-createWeb3Modal({
-  wagmiConfig,
-  projectId: config.walletConnectId,
-  featuredWalletIds: [METAMASK_WALLET_ID, STATUS_WALLET_ID],
+const metadata = {
+    name: "Status Network Bridge",
+    description: `The Status Network Bridge is a bridge solution, providing secure and efficient cross-chain transactions between Ethereum Layer 1 and Status networks.
+  Discover the future of blockchain interaction with Status Network Bridge.`,
+    url: "https://bridge.status.network",
+    icons: [],
+};
+
+createAppKit({
+    adapters: [wagmiAdapter],
+    networks: chains,
+    projectId: config.walletConnectId,
+    metadata,
+    features: {
+        analytics: true,
+        email: false,
+        socials: false,
+        swaps: false,
+        onramp: false,
+        history: false,
+    },
+    enableEIP6963: true,
+    coinbasePreference: "eoaOnly",
+    featuredWalletIds: [METAMASK_WALLET_ID],
 });
 
 type Web3ProviderProps = {
