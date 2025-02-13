f9c5e990147f01ef4a6f0d34b82585512b4116e9
Filip Pajic <42151917+pajicf@users.noreply.github.com>
2025-02-13 14:12:13 +0100

    feat: General UI polishing (#7)
    * sidebar coloring and hover effect updated
    
    * mobile menu background color update
    
    * change app font to inter

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -6,14 +6,14 @@ import { FooterLinks, SocialLinks } from "./footer";
 export default function Sidebar() {
   return (
     <aside id="sidebar" className="fixed left-0 top-0 z-40 hidden h-screen w-52 md:block" aria-label="Sidebar">
-      <div className="bg-dragonPurple flex h-full flex-col justify-between overflow-y-auto py-4">
+      <div className="bg-dragonPurple flex h-full flex-col justify-between overflow-y-auto py-4 " style={{borderTopRightRadius: 32, borderBottomRightRadius: 32}}>
         <div>
           <div className="flex h-24 items-center p-4">
             <Link href="/">
               <Image
-                src={"/images/logo/status-network.svg"}
+                src={"/images/logo/status-network-wrapped.png"}
                 alt="Status Network logo"
-                width={0}
+                width={100}
                 height={0}
                 priority
                 style={{ width: "auto", height: "auto" }}
