aff4d644ae219255206ff9eba6eaf6593d03910b
Filip Pajic <fica.pajic@gmail.com>
2025-02-24 11:29:02 +0100

    add telegram to sidebar

--- src/components/layouts/footer/SocialLinks.tsx
+++ src/components/layouts/footer/SocialLinks.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export const SocialLinks = () => (
   <div className="flex items-center gap-3 py-2">
-    <Link href={"https://x.com/ethstatus"} passHref target="_blank" rel="noopener noreferrer">
-      <Image src={"/images/logo/sidebar/twitter.svg"} alt="Linea logo" width={20} height={16} />
+    <Link href={"https://x.com/statusL2"} passHref target="_blank" rel="noopener noreferrer">
+      <Image src={"/images/logo/sidebar/twitter.svg"} alt="X Logo" width={20} height={16} />
     </Link>
-    <Link href={"https://discord.gg/linea"} passHref target="_blank" rel="noopener noreferrer">
-      <Image src={"/images/logo/sidebar/discord.svg"} alt="Linea logo" width={22} height={16} />
+    <Link href={"https://t.me/statusl2"} passHref target="_blank" rel="noopener noreferrer">
+      <Image src={"/images/logo/sidebar/telegram.svg"} alt="Telegram logo" width={22} height={16} />
     </Link>
   </div>
 );
