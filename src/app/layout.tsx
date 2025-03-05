8a8d34fb7e8f2ddd531beac364ad80e9cf86a90e
Filip Pajic <fica.pajic@gmail.com>
2025-03-05 13:42:11 +0100

    fix: use nextjs metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,25 +7,28 @@ import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 import Fathom from "@/components/fathom/Fathom";
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Status Network Bridge",
   description: `Status Network Bridge is a bridge solution, providing secure and efficient cross-chain transactions between Layer 1 and Status Network.
   Discover the future of blockchain interaction with Status Network Bridge.`,
   icons: {
-    icon: "./favicon.png",
+    icon: "/favicon.ico",
   },
+  openGraph: {
+    images: [
+      {
+        url: "/images/social-card.jpeg",
+        width: "1280",
+        height: "720",
+        alt: "Status Network Bridge Social Card",
+      }
+    ]
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-theme="dark">
-    <title>{metadata.title?.toString()}</title>
-    <meta name="description" content={metadata.description?.toString()} key="desc"/>
-    <meta property="og:image" content="images/social-card.jpeg"/>
-    <meta property="og:image:alt" content="Preview Image for Status Network Bridge"/>
-    <meta property="og:image:type" content="image/jpeg"/>
-    <meta property="og:image:width" content="1280"/>
-    <meta property="og:image:height" content="720"/>
+    <html lang="en" data-theme="light">
 
     <body className={cn(interFont.variable, interFont.className)}>
 
