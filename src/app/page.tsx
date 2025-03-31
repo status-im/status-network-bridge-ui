668ff5c8bdbda705fcb0bd16166cfacd63a30ec1
Filip Pajic <fica.pajic@gmail.com>
2025-03-31 12:10:38 +0200

    fix: main container alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BridgeLayout from "@/components/bridge/BridgeLayout";
 
 export default function Home() {
   return (
-    <div className="min-w-min max-w-lg md:m-auto">
+    <div className="min-w-min max-w-lg m-auto">
       <h1 className="mb-6 text-4xl font-bold md:hidden">Bridge</h1>
       <BridgeLayout />
     </div>
