6142a69f72f09ec14bb27399cd0adc769f0d4c36
JulesFILIOT <dev@julesfiliot.com>
2025-04-23 17:55:20 +0200

    fix: staging env firing analytics

--- src/components/umami/Umami.tsx
+++ src/components/umami/Umami.tsx
@@ -6,6 +6,7 @@ const Umami = () => {
       defer
       src="https://umami.bi.status.im/script.js"
       data-website-id="96a6fed9-77cb-44e8-8aa4-38123e8a62ba"
+      data-domains="bridge.status.network"
     />
   );
 };
