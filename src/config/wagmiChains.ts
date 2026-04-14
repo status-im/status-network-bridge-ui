97bce400ca5fab6f26aafca4c082309a21d6ada4
Filip Pajic <fica.pajic@gmail.com>
2026-04-14 16:59:48 +0200

    chore: use the public rpc endpoint for transport

--- src/config/wagmiChains.ts
+++ src/config/wagmiChains.ts
@@ -9,7 +9,7 @@ export const statusHoodi = { // @TODO temporary, replace with proper wagmi impor
   name: "Status Network Hoodi",
   rpcUrls: {
     default: {
-      http: [process.env.NEXT_PUBLIC_L2_TESTNET_RPC_URL]
+      http: ["https://public.hoodi.rpc.status.network/"]
     }
   },
   blockExplorers: {
